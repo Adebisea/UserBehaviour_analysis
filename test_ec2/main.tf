65e2f96c1e728a9379cbd4f8ec9ef5af1525386d
Adebisea <abdulgkhadijah@gmail.com>
2024-01-04 11:59:30 +0100

    testing airflow,pgadmin and postgres setup on an ec2 instance

---
+++ test_ec2/main.tf
@@ -0,0 +1,79 @@
+#Create ec2 instance
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr_block]
+  }
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220420"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_key_pair" "test_keypair" {
+  key_name           = "userbehav"
+}
+
+
+resource "aws_instance" "ec2_userbehavior" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = data.aws_key_pair.test_keypair.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.name]
+    user_data              = <<EOF
+
+  #!/bin/bash
+
+  echo "-------------------------Setting up dependencies---------------------------"
+  sudo apt-get -y update
+  cd /home/ubuntu && git clone https://github.com/Adebisea/UserBehaviour_analysis.git && cd UserBehaviour_analysis
+  bash test_ec2/toolsetup.sh
+  EOF
+}
